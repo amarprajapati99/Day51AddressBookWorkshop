98f0b38383bcf9945fba536b4b7f8a0a83580175
amarprajapati99 <amarprajapati99@gmail.com>
2021-05-22 08:22:52 +0530

    Added : Update address book contact details

--- Js/addressBook.js
+++ Js/addressBook.js
@@ -1,4 +1,4 @@
-class AddressBook {
+class AddressBook{
 
     get id() {
         return this._id;
@@ -7,45 +7,53 @@ class AddressBook {
         this._id = value;
     }
 
-
-    get fullName() { return this._fullName; }
-    set fullName(fullName) {
-        let fullNamePattern = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-        if (fullNamePattern.test(fullName)) {
-            this._fullName = fullName;
-        }
-        else
-            throw 'Invalid Name';
-    }
     get fullName() {
         return this._fullName;
     }
-
+    set fullName(name) {
+        let names=name.split(" ");
+        
+        if(names.length==2){
+            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+            if(!nameRegex.test(names[0])) throw "First name is Incorrect";
+            if(!nameRegex.test(names[1])) throw "Last name is Incorrect";
+            if(nameRegex.test(names[0])&& nameRegex.test(names[1])) {
+                this._fullName = name;
+            }
+            else throw "Full name is Incorrect";
+        }
+        else{
+            let nameRegex=RegExp('^[A-Z][a-z]{2,}$');
+            if(!nameRegex.test(names[0])) throw "First Name is Invalid";
+            else this._fullName = value;
+        }
+        
+    }
     get address() {
         return this._address;
     }
     set address(address) {
-        let words = address.split(" ");
-        if (words.length > 1) {
-            let addressRegex = RegExp('^[A-Za-z,.0-9]{3,}$');
-            for (let word of words) {
-                if (!addressRegex.test(word)) throw "Please enter atleast 3 letters";
-                else this._address = address;;
-            }
-        }
-        else throw "Please enter multiple words";
+         let words=address.split(" ");
+         if(words.length>1){
+             let addressRegex=RegExp('^[A-Za-z,.0-9]{3,}$');
+             for(let word of words){
+                 if(!addressRegex.test(word)) throw "Please enter atleast 3 letters";
+                 else this._address = address;;
+             }
+         }
+         else throw "Please enter multiple words";
     }
     get mobileNumber() {
         return this._mobileNumber;
     }
     set mobileNumber(phone) {
 
-        let phoneRegex1 = RegExp('^[1-9][0-9]{9}$');
-        let phoneRegex2 = RegExp('^[0-9]{2}[1-9][0-9]{9}$');
-        let phoneRegex3 = RegExp('^[+][0-9]{2}[1-9][0-9]{9}$');
-        if (phoneRegex1.test(phone) || phoneRegex2.test(phone) || phoneRegex3.test(phone)) this._mobileNumber = phone;
+        let phoneRegex1=RegExp('^[1-9][0-9]{9}$');
+        let phoneRegex2=RegExp('^[0-9]{2}[1-9][0-9]{9}$');
+        let phoneRegex3=RegExp('^[+][0-9]{2}[1-9][0-9]{9}$');
+        if(phoneRegex1.test(phone)||phoneRegex2.test(phone)||phoneRegex3.test(phone)) this._mobileNumber = phone;
         else throw "Phone number is Incorrect";
-
+        
     }
     get city() {
         return this._city;
@@ -66,8 +74,9 @@ class AddressBook {
         this._zip = value;
     }
 
-    toString() {
-        return "fullName " + this._fullName + "mobileNumber " + this._mobileNumber + "address " + this._address + " city " +
-            this.city + "state " + this.state + "zip " + this.zip;
+    toString(){
+        return "fullName "+this._fullName+"mobileNumber "+this._mobileNumber+"address "+this._address+" city "+
+        this.city+"state "+this.state+"zip "+this.zip;
     }
+
 }
