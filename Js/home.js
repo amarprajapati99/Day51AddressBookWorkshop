052e73c539e6f9ac8e5fd4da1962f6e65bb3f1d5
amarprajapati99 <amarprajapati99@gmail.com>
2021-05-22 05:37:39 +0530

    Added : Address book list form local storage

--- Js/home.js
+++ Js/home.js
@@ -15,4 +15,23 @@ const createInnerHtml=()=>{
     </tr>`;
     let innerHtml=`${headerHtml}`;
     document.querySelector('#table-display').innerHTML=innerHtml;
+    if(addressBookList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for ( const addressBookData of addressBookList) {
+        innerHtml = `${innerHtml}
+        <tr class="trr">
+            <td>${addressBookData._fullName}</td>
+            <td>${addressBookData._address}</td>
+            <td>${addressBookData._city}</td>
+            <td>${addressBookData._state}</td>
+            <td>${addressBookData._zip}</td>
+            <td>${addressBookData._mobileNumber}</td>
+            <td>
+                <img id="${addressBookData._id}" onclick="remove(this)" alt="delete" src="../Assets/delete-black-18dp.svg">
+                <img id="${addressBookData._id}" onclick="update(this)" alt="edit" src="../Assets/create-black-18dp.svg">
+            </td>
+        </tr>
+    `;
+    }
+        document.querySelector('#table-display').innerHTML = innerHtml;
 }
